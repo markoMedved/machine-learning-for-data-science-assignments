366cd69785d1fd08eb9999ef8c945c74dbcdb43a
markoMedved <marko.medved2002@gmail.com>
2025-04-18 21:33:38 +0200

    started neural networks

--- Artificial-neural-networks/nn.py
+++ Artificial-neural-networks/nn.py
@@ -1,17 +1,65 @@
 import numpy as np
 import csv
 
+def init_glorot(all_units):
+    # Initialize the weights, using glorot initialization
+    weights_all_layers = []
+    for i in range(len(all_units) - 1):
+        scale = np.sqrt(1 / (all_units[i]))
+        weights_all_layers.append(
+            np.random.normal(loc=0, scale=scale, size=(all_units[i+1], all_units[i]))
+        )
+    return weights_all_layers
+
+def sigmoid(x):
+    return 1 / (1+ np.exp(-x))
+
+def softmax(X, x):
+    return np.exp(x)/np.sum(np.exp(X)) 
+
+def forward_pass(weights_all_layers, X):
+    # TODO probably also gonna need to save the activations, for GD
+    probs = []
+    for current_activations in X.copy():
+        for i, weights in enumerate(weights_all_layers):
+            current_activations = weights @ current_activations
+            current_activations = np.array([sigmoid(x) for x in current_activations])
+
+        current_activations = [softmax(current_activations, x_k) for x_k in current_activations]
+        probs.append(current_activations)
+
+    return np.array(probs)
+
 
 class ANNClassification:
     # Units determine the amount of activations in each hidden layer
     def __init__(self, units, lambda_):
         self.units = units
         
-    def fit(X,y):
+    def fit(self,X,y, seed = 42):
+        np.random.seed(seed)
+    
+        # Get classes andd numbers of classes
+        classes = np.unique(y)
+        num_classes = len(classes)
+
+        # Add input and output layer units
+        self.all_units = [len(X[0])]
+        self.all_units.extend(self.units)
+        self.all_units.append(num_classes)
+
+        # Initialize the weights, using glorot initialization
+        weights_all_layers = init_glorot(self.all_units)
+
+        # Forward pass
+        probs = forward_pass(weights_all_layers, X)
+        print(probs)
+        
+
         
 
 class ANNClassificationPredict:
-
+    pass
 
 class ANNRegression:
     # implement me too, please
@@ -44,8 +92,9 @@ def squares():
 
 if __name__ == "__main__":
 
+
     # example NN use
-    fitter = ANNClassification(units=[3,4], lambda_=0)
+    fitter = ANNClassification(units=[3,6,4], lambda_=0)
     X = np.array([
         [1, 2, 3],
         [4, 5, 6],
